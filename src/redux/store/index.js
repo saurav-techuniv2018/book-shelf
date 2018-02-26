f04830301e8b548af895b6ea312e7a3656840903
Sahu, S <sauravsahu.12@live.com>
2018-02-26 09:15:08 +0530

    Add redux devtools config.
    Add redux devtools config.

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import * as reducers from '../reducers';
 
-const initStore = () => createStore(combineReducers(reducers));
+/* eslint-disable no-underscore-dangle */
+const initStore = () =>
+  createStore(
+    combineReducers(reducers),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
+/* eslint-enable */
 
 export default initStore;
